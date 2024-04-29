47de01ab63c2f26dec00daf182e2a53c82b25631
Pavlo-Goncharenko <82305213+Pavlo-Goncharenko@users.noreply.github.com>
2024-04-30 00:33:04 +0300

    0032

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+
+
+// HIDE STICKER:
+// Find the element with id="stickerClose"
+const stickerClose = document.getElementById('stickerClose');
+
+// Check if the element exists and add a click event listener
+if (stickerClose) {
+	stickerClose.addEventListener('click', function () {
+		// Find the element with id="sticker"
+		const sticker = document.getElementById('sticker');
+
+		// Check if the sticker element exists and add the class "hide-sticker"
+		if (sticker) {
+			sticker.classList.add('hide-sticker');
+		}
+	});
+}
